a217f1b9b7aec2c0ccd98270cb7894241de323bb
Harshit <harshit.owo@gmail.com>
2024-07-09 11:22:16 +0530

    ✨ func pointer in _read_until

--- src/include/utils.h
+++ src/include/utils.h
@@ -5,5 +5,6 @@ const char* find_path(int mode);
 FILE* get_file(int mode);
 char* read_line(FILE* file);
 char* read_until(FILE* file ,char terminator);
+char* _read_until(FILE* file ,int (*comparator)(int));
 void free_line(char* line_data);
 #endif
--- src/utils.c
+++ src/utils.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 const char* find_path(int mode){
     return mode == 0 ? "data/test.txt" : "data/input.txt";
 }
@@ -19,6 +20,33 @@ char* read_until(FILE* file ,char terminator){
     char* buffer = malloc(buffer_size);
     assert(buffer != NULL);
     while(c = fgetc(file) , c != terminator && c != EOF){
+        if (offset == buffer_size - 1){
+            buffer_size *=2;
+            buffer = realloc(buffer, buffer_size);
+            assert(buffer != NULL);
+        }
+        
+        buffer[offset++] = (char)c;
+    }
+    if(c == EOF && offset == 0){
+        free(buffer);
+        return NULL;
+    }
+    if (offset < buffer_size-1) {
+        buffer = realloc(buffer,offset +1);
+        assert(buffer != NULL);
+    }
+    buffer[offset] = '\0';
+    return buffer;
+}
+
+char* _read_until(FILE* file ,int (*comparator)(int)){
+    int c;
+    int offset = 0;
+    int buffer_size = sizeof(char) * 4;
+    char* buffer = malloc(buffer_size);
+    assert(buffer != NULL);
+    while(c = fgetc(file) , comparator(c) && c != EOF){
         if (offset == buffer_size - 1){
             buffer_size *=2;
             buffer = realloc(buffer, buffer_size);
